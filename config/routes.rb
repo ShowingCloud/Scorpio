c70b2c9aa8f79aaff726b0531b86ab1d04398bd9
WANG Guoqin <wangguoqin1001@gmail.com>
2012-11-15 05:04:17 +0800

    Flexible robots.txt for both development and production

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Refinerycms::Application.routes.draw do
 	  end
   end
 
+  get '/robots.txt' => 'application#robots'
+
   # This line mounts Refinery's routes at the root of your application.
   # This means, any requests to the root URL of your application will go to Refinery::PagesController#home.
   # If you would like to change where this extension is mounted, simply change the :at option to something different.
