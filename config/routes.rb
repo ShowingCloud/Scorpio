fa7282ff5c2ae9334b31ea4cf7c7d756d73fc205
WANG Guoqin <wangguoqin1001@gmail.com>
2012-11-29 02:45:06 +0800

    Redirect /admin to /refinery

--- config/routes.rb
+++ config/routes.rb
@@ -49,6 +49,8 @@ Refinerycms::Application.routes.draw do
 
   get '/robots.txt' => 'application#robots'
 
+  get 'admin', :to => redirect('/refinery')
+
   # This line mounts Refinery's routes at the root of your application.
   # This means, any requests to the root URL of your application will go to Refinery::PagesController#home.
   # If you would like to change where this extension is mounted, simply change the :at option to something different.
