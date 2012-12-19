811efdf4ec0bd342c3298471ec17e50ef27c997c
WANG Guoqin <wangguoqin1001@gmail.com>
2012-12-20 07:21:49 +0800

    Localities: database

--- db/migrate/20121218215500_add_freight_columns_for_localities.rb
+++ db/migrate/20121218215500_add_freight_columns_for_localities.rb
@@ -1,6 +1,6 @@
 class AddFreightColumnsForLocalities < ActiveRecord::Migration
 	def up
-		add_column :localities, :freight_paid, :decimal, :precision => 5, :scale => 2, :default => 10
+		add_column :localities, :freight_paid, :decimal, :precision => 5, :scale => 2, :default => 20
 		add_column :localities, :freight_cod, :decimal, :precision => 5, :scale => 2, :default => 20
 	end
 
